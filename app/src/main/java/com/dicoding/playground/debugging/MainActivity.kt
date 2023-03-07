3844be828757ce394549059f685f3af7263470f6
Irshal Firdeansyah <irshalfir@gmail.com>
2023-03-07 07:56:59 +0700

    add fix bug at NPE error scenario

--- app/src/main/java/com/dicoding/playground/debugging/MainActivity.kt
+++ app/src/main/java/com/dicoding/playground/debugging/MainActivity.kt
@@ -8,15 +8,17 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var btnSetValue: Button? = null
+    private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSetValue = findViewById(R.id.btn_set_value)
         tvText = findViewById(R.id.tv_text)
-        btnSetValue!!.setOnClickListener(this)
+
+        btnSetValue.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
