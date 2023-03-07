77a33f574570821e1af6c629ccc389122b2e202e
Irshal Firdeansyah <irshalfir@gmail.com>
2023-03-07 07:55:20 +0700

    add NPE scenarion

--- app/src/main/java/com/dicoding/playground/debugging/MainActivity.kt
+++ app/src/main/java/com/dicoding/playground/debugging/MainActivity.kt
@@ -2,10 +2,27 @@ package com.dicoding.playground.debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var btnSetValue: Button? = null
+    private lateinit var tvText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue!!.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_set_value) {
+            tvText.text = "19"
+        }
+    }
+
 }
